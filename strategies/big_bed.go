8d866f44dd360a045ddedee4d97945c9812e801e
Rozhnov Alexandr <nox73@ya.ru>
2016-04-24 12:17:55 +0400

    Fix.

--- strategies/big_bed.go
+++ strategies/big_bed.go
@@ -6,7 +6,7 @@ import "github.com/lean-poker/poker-player-go/leanpoker"
 func checkBigBed(game *leanpoker.Game) bool {
 	player := game.Players[game.InAction]
 
-	result := game.CurrentBuyIn > player.Stack*0.3
+	result := float64(game.CurrentBuyIn) > float64(player.Stack)*0.3
 	if result {
 		log.Printf("Got big bet. %v %v", game.CurrentBuyIn, player.Stack)
 	}
