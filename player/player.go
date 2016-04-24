d5fc35e16af8b899c56358a05c7dc9f036572007
Andrew Kulakov <chaos.kool@gmail.com>
2016-04-24 10:41:50 +0400

    Update player.go

--- player/player.go
+++ player/player.go
@@ -5,7 +5,7 @@ import "github.com/lean-poker/poker-player-go/leanpoker"
 const VERSION = "Default Go folding player"
 
 func BetRequest(state *leanpoker.Game) int {
-	return 0
+	return 1;
 }
 
 func Showdown(state *leanpoker.Game) {
