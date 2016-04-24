747d47cd3a6982efafceef9b2a9a8c9925868ba8
Rozhnov Alexandr <nox73@ya.ru>
2016-04-24 11:25:07 +0400

    Fixed.

--- strategies/pair.go
+++ strategies/pair.go
@@ -11,10 +11,20 @@ const (
 func checkPair(state) (int, bool) {
 	player := state.Players[state.InAction]
 
-	log.Printf("Check own pair %v %v", player.HoleCards[0].Rank, player.HoleCards[1].Rank)
+	log.Printf("Check pair %v %v", player.HoleCards, state.CommunityCards)
 
 	if player.HoleCards[0].Rank == player.HoleCards[1].Rank {
+		log.Printf("Got own pair")
 		return player.Raise(game.SmallBlind, game.CurrentBuyIn, BigPairRank), true
+	}
+
+	for _, c := range player.HoleCards {
+		for _, cc := range state.CommunityCards {
+			if c.Rank == cc.Rank {
+				log.Printf("Got community pair")
+				return player.Raise(game.SmallBlind, game.CurrentBuyIn, MiddlePairRank), true
+			}
+		}
 	}
 
 	return 0, false
